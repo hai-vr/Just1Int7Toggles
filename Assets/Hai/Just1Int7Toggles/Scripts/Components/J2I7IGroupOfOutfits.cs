8b4a8fee34c7ff9c1c853862abec4f0029cdb701
Haï~ <hai-vr@users.noreply.github.com>
2020-08-17 19:25:47 +0200

    WIP

---
+++ Assets/Hai/Just1Int7Toggles/Scripts/Components/J2I7IGroupOfOutfits.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDK3.Avatars.ScriptableObjects;
+
+namespace Hai.Just1Int7Toggles.Scripts.Components
+{
+    public class J2I7IGroupOfOutfits : MonoBehaviour
+    {
+        public string name;
+        public Image picture;
+        
+        public List<J2I7IOutfit> outfits;
+        public int indexDefaultOn;
+
+        public VRCExpressionsMenu menu0to7;
+        public VRCExpressionsMenu menu7to15;
+    }
+    
+    [Serializable]
+    public struct J2I7IOutfit
+    {
+        public string name;
+        public Image picture;
+        
+        public List<GameObject> items;
+    }
+}
---
+++ Assets/Hai/Just1Int7Toggles/Scripts/Components/Just2Ints7IntsCompiler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using VRC.SDK3.Avatars.Components;
+
+namespace Hai.Just1Int7Toggles.Scripts.Components
+{
+    public class Just2Ints7Ints : MonoBehaviour
+    {
+        public RuntimeAnimatorController animatorController;
+        public AnimationClip customEmptyClip;
+        
+        public VRCAvatarDescriptor avatar;
+
+        private List<J2I7IGroupOfOutfits> outfits;
+    }
+}
